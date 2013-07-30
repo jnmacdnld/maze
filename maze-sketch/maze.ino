3b385a30726c4464d8467e044165b69b030fb79d
John Macdonald <jnmacdnld@gmail.com>
2013-07-30 10:14:04 -0400

    tabs to spaces

--- maze-sketch/maze.ino
+++ maze-sketch/maze.ino
@@ -1,29 +1,29 @@
 #include "MazeTextfileProcessor.cpp"
 
 void setup() {
-	Serial.begin(9600);
-	/*
-	String wallString = "+-+-+ + +-+-+-+-+-+-+ + +-+ +-+ +";
+  Serial.begin(9600);
+  /*
+  String wallString = "+-+-+ + +-+-+-+-+-+-+ + +-+ +-+ +";
   uint16_t wallInt = 
-  	MazeTextfileProcessor::horizontalWallStringToInt(wallString);
-	String backToWallString = 
-		MazeTextfileProcessor::intToHorizontalWallString(wallInt);
+    MazeTextfileProcessor::horizontalWallStringToInt(wallString);
+  String backToWallString = 
+    MazeTextfileProcessor::intToHorizontalWallString(wallInt);
 
-	Serial.println("there...     " + wallString);
-	Serial.println("and back.    " + backToWallString);
-	*/
+  Serial.println("there...     " + wallString);
+  Serial.println("and back.    " + backToWallString);
+  */
 
-	String wallString = "| |   | |     | | |             |";
-	uint16_t wallArr[15];
-	uint8_t row = 4;
+  String wallString = "| |   | |     | | |             |";
+  uint16_t wallArr[15];
+  uint8_t row = 4;
   MazeTextfileProcessor::setIntArrayFromVerticalWallString(wallArr,
-  																												 wallString,
-  																												 row);
-	String backToWallString = 
-		MazeTextfileProcessor::intArrayToVerticalWallString(wallArr, row);
+                                                           wallString,
+                                                           row);
+  String backToWallString = 
+    MazeTextfileProcessor::intArrayToVerticalWallString(wallArr, row);
 
-	Serial.println("there...     " + wallString);
-	Serial.println("and back.    " + backToWallString);
+  Serial.println("there...     " + wallString);
+  Serial.println("and back.    " + backToWallString);
 }
 
 void loop() {
