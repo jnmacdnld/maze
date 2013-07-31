557c057fd7b740d112f81e808747e153df2aab5d
John Macdonald <jnmacdnld@gmail.com>
2013-07-30 20:59:53 -0400

    Revert "got rid of that README on second thought"
    This reverts commit a6a84ab869b038eac4d9acb3380709b9af1f7f62.

--- maze_sketch/maze_sketch.ino
+++ maze_sketch/maze_sketch.ino
@@ -5,15 +5,15 @@ void setup() {
 
   Serial.println("Begin");
 
-  char backToWallString[ ] = "+ + + + + + + + + + + + + + + + +";
+  /*char backToWallString[MAZE_STRING_LENGTH_NULL_T];
   char wallString[ ] = "+-+-+ + +-+-+-+-+-+-+ + +-+ +-+ +";
 
   Serial.println("Call horizontalWallStringToInt");
   uint16_t wallInt = 
     MazeTextfileProcessor::horizontalWallStringToInt(wallString);
 
-  Serial.println("Call setStringFromHorizontalWallInt");
-  MazeTextfileProcessor::setStringFromHorizontalWallInt(
+  Serial.println("Call initStringFromHorizontalWallInt");
+  MazeTextfileProcessor::initStringFromHorizontalWallInt(
                                                     backToWallString, wallInt);
 
   Serial.print("There...     ");
@@ -22,19 +22,28 @@ void setup() {
 
   Serial.print("And back.    ");
   Serial.print(backToWallString);
-  Serial.println();
+  Serial.println();*/
 
-/*  String wallString = "| |   | |     | | |             |";
+  char wallString[ ] = "| |   | |     | | |             |";
+  char backToWallString[MAZE_STRING_LENGTH_NULL_T];
+
   uint16_t wallArr[15];
   uint8_t row = 4;
+
   MazeTextfileProcessor::setIntArrayFromVerticalWallString(wallArr,
                                                            wallString,
                                                            row);
-  String backToWallString = 
-    MazeTextfileProcessor::intArrayToVerticalWallString(wallArr, row);
 
-  Serial.println("there...     " + wallString);
-  Serial.println("and back.    " + backToWallString);*/
+  MazeTextfileProcessor::initVerticalWallStringFromIntArray(backToWallString,
+                                                            wallArr, row);
+
+  Serial.print("There...     ");
+  Serial.print(wallString);
+  Serial.println();
+
+  Serial.print("And back.    ");
+  Serial.print(backToWallString);
+  Serial.println();
 }
 
 void loop() {
